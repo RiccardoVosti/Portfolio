c3e9b22f45b4c7546a5539d80045d6094cb80903
RiccardoVosti <vosti.riccardo@gmail.com>
2026-02-02 15:28:36 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,50 +27,7 @@ function windowResized() {
     container.clientWidth,
     container.clientHeight
   );
-  calculateGrid();
+ 
 }
 
 
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
-
-document.querySelectorAll("a").forEach(link => {
-  const originalText = link.textContent;
-  const letters = originalText.split("");
-
-  // sostituisce il testo con span
-  link.textContent = "";
-  const spans = letters.map(letter => {
-    const span = document.createElement("span");
-    span.textContent = letter;
-    link.appendChild(span);
-    return span;
-  });
-
-  let animating = false;
-
-  link.addEventListener("mouseenter", () => {
-    if (animating) return;
-    animating = true;
-
-    spans.forEach((span, i) => {
-      let iterations = 0;
-
-      const interval = setInterval(() => {
-        span.textContent =
-          chars[Math.floor(Math.random() * chars.length)];
-
-        iterations++;
-
-        if (iterations > 5) {
-          span.textContent = letters[i];
-          clearInterval(interval);
-
-          if (i === spans.length - 1) {
-            animating = false;
-          }
-        }
-      }, 25 + i * 15); // ← ordered offset
-    });
-  });
-
-});
