ae90065918ebff0bd09c7f37c1389aa1e22b17a7
RiccardoVosti <vosti.riccardo@gmail.com>
2026-02-06 12:01:32 +0100

    readded nav-animation to es3

--- es3/sketch.js
+++ es3/sketch.js
@@ -241,3 +241,33 @@ function windowResized() {
   btnReset.hide();
   btnBoom.show();
 }
+
+
+
+// Hacker Effect logic remains the same
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("a,.logotesto");
+  const GLYPHS = "X#%&@$01+-*/<>[]{}☺";
+
+  links.forEach((link) => {
+    const originalText = link.innerText;
+    let interval = null;
+    link.addEventListener("mouseenter", () => {
+      let iteration = 0;
+      clearInterval(interval);
+      interval = setInterval(() => {
+        link.innerText = originalText.split("").map((l, i) => {
+          if (i < iteration) return originalText[i];
+          if (l === " ") return " ";
+          return GLYPHS[Math.floor(Math.random() * GLYPHS.length)];
+        }).join("");
+        if (iteration >= originalText.length) clearInterval(interval);
+        iteration += 1;
+      }, 40);
+    });
+    link.addEventListener("mouseleave", () => {
+      clearInterval(interval);
+      link.innerText = originalText;
+    });
+  });
+});
